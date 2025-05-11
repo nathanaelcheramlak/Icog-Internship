b4062ccdc1151539cc67a2b79774ce47008a1e7e
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-05-11 23:52:11 +0300

    minor update

--- Trading-Task/task_one/utils.py
+++ Trading-Task/task_one/utils.py
@@ -1,10 +1,9 @@
 import yfinance as yf
-import numpy as np
-import pandas as pd
 
-def download_trading_data(trinker="EURUSD=X", period="1mo", interval="15m"):    
-    data = yf.download(trinker, period, interval)
+def download_trading_data(tickers="EURUSD=X", period="1mo", interval="5m"):   
+    data = yf.download(tickers=tickers, period=period, interval=interval)
     data.to_csv("trading_data.csv")
 
-offline_data = pd.read_csv('trading_data.csv')
-print(offline_data.count())
+    print('Data Saved.')
+
+download_trading_data()
